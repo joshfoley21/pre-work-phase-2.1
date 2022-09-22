e31fbf86814abf725f12acb08b74dcca9b154c0f
joshfoley21 <113781712+joshfoley21@users.noreply.github.com>
2022-09-22 08:28:46 -0600

    Add files via upload

---
+++ conditionals-and-functions.js
@@ -0,0 +1,55 @@
+let equals = 1 === 1;
+console.log(equals)
+let greaterThan = 5 > 1;
+let lessThan = 2 < 10;
+let greaterThanEq = 5 >= 5;
+let lessThanEq = 4 <= 9;
+let notEquals = 5 !== 2;
+
+let storeA = 4.40;
+let storeB = 4.4;
+
+function compareStorePrices(storeA, StoreB) {
+    let storeAIsLower = storeA <storeB;
+    console.log(storeAIsLower)
+
+}
+if (storeAIsLower) {
+    console.log("store A has a lower price.")
+} else if (storeB <storeA) {
+    console.log("Store B has a Lower price.")
+}else {
+    console.log("Their prices are equal.")
+}
+compareStorePrices(10, 5);
+compareStorePrices(7,10);
+
+function squareNum(number) {
+    let squared = number * number;
+    return squared = number - 19;
+}
+let squaredNumber = squareNum(7);
+console.log(squaredNumber);
+
+//
+
+let x = 10;
+function addNumbers (n, m) {
+    console.log(x)
+    if (1===1) {
+        let b = 8;
+    }
+    console.log(b);
+    return n + m;
+}
+addNumbers(2, 3);
+console.log(x);
+
+addNumbers(1, 2, 10);
+
+////
+
+let ourArray = [1, 2, 3, 4, 5];
+console.log(ourArray[0]);
+
+/*...*/
---
+++ objects-apis.js
@@ -0,0 +1,18 @@
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "Small",
+    bark: function (typeOfBark){
+    },
+};
+function x(y) {
+    y = y + 5;
+    console.log(y);
+}
+
+let y = 5;
+    name: "Tom",
+        num: 10,
+ x(y);
+ console.log(y);
